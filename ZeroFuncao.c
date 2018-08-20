f1c26e77a0263430968e6fb6901df8b0d9b4a0a4
Schatzmann <annelyse94@gmail.com>
2018-08-20 00:52:32 -0300

    funções

--- ZeroFuncao.c
+++ ZeroFuncao.c
@@ -7,14 +7,18 @@
 #include "ZeroFuncao.h"
 
 double bisseccao (Polinomio p, double a, double b, double eps, int *it, double *raiz){
-	double xm, pxm, dpxm, xmn, pxmn, dpxmn, pa, dpa;
+	double xm, pxm, dpxm, xmn, pxmn, dpxmn, pa, dpa, erro;
+	int inte = 1; // ja começa com uma iteração
 
 	xmn = (a + b) / 2;
 
 	do {
 		xm = xmn;
+
+		calcPolinomio_rapido(p, a, &pa, &dpa);
+		calcPolinomio_rapido(p, xmn, &pxmn, &dpxmn);
 		
-		if((calcPolinomio_rapido(p, a, &pa, &dpa) * calcPolinomio_rapido(p, xmn, &pxmn, &dpxmn)) > ZERO){
+		if((pa * pxmn) > ZERO){
 			a = xm;
 		} 
 		else {
@@ -22,32 +26,43 @@ double bisseccao (Polinomio p, double a, double b, double eps, int *it, double *
 		}
 
 		xmn = (a + b) / 2;
-	} while(fabs((xmn - xm) / xmn) > eps);
+		erro = fabs((xmn - xm) / xmn);
+		inte ++;
 
-	raiz = xmn;
+	} while(erro > eps && inte < MAXIT);
 
-	return 0;
+
+	*raiz = xmn;
+	*it = inte;
+
+	return erro;
 }
 
 
 double newtonRaphson (Polinomio p, double x0, double eps,int *it, double *raiz){
-	double x, px0, dpx0;
+	double x, px0, dpx0, erro;
+	int inte = 1;
 
 	calcPolinomio_rapido(p, x0, &px0, &dpx0);
 
-	if(dpx0 != ZERO){
+	if(dpx0 != ZERO){  //derivada = 0;
 		
 		x = x0 - px0 / dpx0;
+		erro = fabs((x - x0) / x);
 
-		while(fabs((x - x0) / x) > eps){
+		while(erro > eps && inte < MAXIT){
 			x0 = x;
 			calcPolinomio_rapido(p, x0, &px0, &dpx0);
 			x = x0 - px0 / dpx0;
+
+			erro = fabs((x - x0) / x);
+			inte++;
 		}
 
-		raiz = x;
+		*raiz = x;
+		*it = inte;
 
-		return 0;
+		return erro;
 	}
 
 	return -1;
@@ -55,30 +70,120 @@ double newtonRaphson (Polinomio p, double x0, double eps,int *it, double *raiz){
 
 
 double secante (Polinomio p, double x0, double x1, double eps, int *it, double *raiz){
-	double x, px0, dpx0, px1, dpx1;
+	double x, px0, dpx0, px1, dpx1, erro;
+	int inte = 1;
 
 	calcPolinomio_rapido(p, x0, &px0, &dpx0);
 	calcPolinomio_rapido(p, x1, &px1, &dpx1);
 		
 	x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+	erro = fabs((x - x0) / x);
 
-	while(fabs((x - x0) / x) > eps){
+	while(erro > eps && inte < MAXIT){
 		x0 = x1;
 		x1 = x;
 		calcPolinomio_rapido(p, x0, &px0, &dpx0);
 		calcPolinomio_rapido(p, x1, &px1, &dpx1);
 		
 		x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+		erro = fabs((x - x0) / x);	
+		inte++;
 	}
 
-	raiz = x;
+	*raiz = x;
+	*it = inte;
 
-	return 0;
+	return erro;
 }
 
 
+double bisseccao_lento(Polinomio p, double a, double b, double eps, int *it, double *raiz){
+	double xm, pxm, dpxm, xmn, pxmn, dpxmn, pa, dpa, erro;
+	int inte = 1; // ja começa com uma iteração
 
-void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx){
+	xmn = (a + b) / 2;
+
+	do {
+		xm = xmn;
+
+		calcPolinomio_lento(p, a, &pa, &dpa);
+		calcPolinomio_lento(p, xmn, &pxmn, &dpxmn);
+		
+		if((pa * pxmn) > ZERO){
+			a = xm;
+		} 
+		else {
+			b = xm;
+		}
+
+		xmn = (a + b) / 2;
+		erro = fabs((xmn - xm) / xmn);
+		inte ++;
+
+	} while(erro > eps && inte < MAXIT);
+
+
+	*raiz = xmn;
+	*it = inte;
+
+	return erro;
+}
+
+double newtonRaphson_lento (Polinomio p, double x0, double eps,int *it, double *raiz){
+	double x, px0, dpx0, erro;
+	int inte = 1;
+
+	calcPolinomio_lento(p, x0, &px0, &dpx0);
+
+	if(dpx0 != ZERO){  //derivada = 0;
+		
+		x = x0 - px0 / dpx0;
+		erro = fabs((x - x0) / x);
+
+		while(erro > eps && inte < MAXIT){
+			x0 = x;
+			calcPolinomio_lento(p, x0, &px0, &dpx0);
+			x = x0 - px0 / dpx0;
+
+			erro = fabs((x - x0) / x);
+			inte++;
+		}
+
+		*raiz = x;
+		*it = inte;
+
+		return erro;
+	}
+}
+
+double secante_lento (Polinomio p, double x0, double x1, double eps, int *it, double *raiz){
+	double x, px0, dpx0, px1, dpx1, erro;
+	int inte = 1;
+
+	calcPolinomio_lento(p, x0, &px0, &dpx0);
+	calcPolinomio_lento(p, x1, &px1, &dpx1);
+		
+	x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+	erro = fabs((x - x0) / x);
+
+	while(erro > eps && inte < MAXIT){
+		x0 = x1;
+		x1 = x;
+		calcPolinomio_lento(p, x0, &px0, &dpx0);
+		calcPolinomio_lento(p, x1, &px1, &dpx1);
+		
+		x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+		erro = fabs((x - x0) / x);	
+		inte++;
+	}
+
+	*raiz = x;
+	*it = inte;
+
+	return erro;
+}
+
+
 void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx){
 	double poli, deri;
 	int k;
@@ -86,7 +191,7 @@ void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx){
 	poli = p.p[p.grau];
 	deri = poli;
 
-	for(k = p.grau - 1; k; k--){
+	for(k = p.grau - 1; k; --k){
 		poli = p.p[k] + poli * x;
 		deri = poli + deri * x;
 	}
@@ -99,16 +204,17 @@ void calcPolinomio_rapido(Polinomio p, double x, double *px, double *dpx){
 
 
 void calcPolinomio_lento(Polinomio p, double x, double *px, double *dpx){
-	double poli = p.p[p.grau];
+	double poli = 0;
  	double deri = poli;
  	int k;
 
- 	for(k = p.grau-1; k; k--){
- 		poli = pow(p.p[k], k) + poli * x;
- 		deri = poli + deri * x;
-  	}
+ 	for(k = p.grau; k; --k){
+ 		poli = p.p[k] * pow(x, k) + poli;
+ 		deri = (p.p[k] * k) * pow(x, k-1) + deri;
+  }
 
-  	poli = p.p[0] + poli * x;
-  	*px = poli;
-  	*dpx = deri;
+  poli = p.p[0] * pow(x,0) + poli;
+
+  *px = poli;
+  *dpx = deri;
 }
